d015035365a7009a3e602ca26a30cdad80d9d3d6
luckyfengyong <fengyong.gm@gmail.com>
2015-03-14 22:16:52 -0400

    add node.js

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -9,12 +9,54 @@ function disableFirewall {
 
 function installUtilities {
 	echo "install utilities"
+	# for mongodb
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+	echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 	apt-get update
-	apt-get -y install docker.io
-	apt-get -y install zip
-	# service docker.io start
+	apt-get -y install docker.io zip 
+	# install nginx as reverse proxy https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-14-04
+	apt-get -y install nginx
+	# install node.js npm
+	apt-get -y install build-essential openssl libssl-dev pkg-config
+	apt-get -y install nodejs nodejs-dev npm
+	ln -s /usr/bin/nodejs  /usr/bin/node
+	# install and build latest version of nodejs
+	# if resourceExists /vagrant/resources/node-v0.12.0.tar.gz; then
+	#	echo "install node.js from local file"
+	# else
+	#	curl -o /vagrant/resources/node-v0.12.0.tar.gz -O -L http://nodejs.org/dist/v0.12.0/node-v0.12.0.tar.gz
+	# fi
+	# tar -xzf /vagrant/resources/node-v0.12.0.tar.gz -C /usr/local
+	# ln -s /usr/local/node-v0.12.0 /usr/local/node
+	# cd /usr/local/node
+	# ./configure
+	# make
+	# make install
+    # install the latest version of npm
+	# curl -o /vagrant/resources/install.sh -O --insecure -L https://npmjs.org/install.sh
+	# /vagrant/resources/install.sh
+	# configure the npm path
+	npm config set prefix "/usr/local/"
+	# npm config set cache "/usr/local/node-cache"
+	# install node.js app of http-server https://github.com/nodeapps/http-server
+    # npm install -g http-server
+
+	# install the latest version of express (node.js framework http://expressjs.com/) exmaple http://www.cnblogs.com/zhongweiv/p/nodejs_express.html
+	npm install -g express
+	npm install -g express-generator
+	# apt-get -y install node-express
+	# create a project
+	# express -e demoapp
+	# install dependence
+	# cd demoapp && npm install
+	# run the app
+	# DEBUG=demoapp:* ./bin/www
+	# install mongodb for node.js http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+	apt-get install -y mongodb-org
+	npm install -g session-mongoose
+	npm install -g --save express-session cookie-parser
 }
-echo "setup centos"
+echo "setup ubuntu"
 
 disableFirewall
 installUtilities
